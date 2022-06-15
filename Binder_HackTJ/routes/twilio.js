dbe888c6ed08c30cee75f3abf2ef01c1a425efd3
allenh99 <72663178+allenh99@users.noreply.github.com>
2022-06-15 08:11:54 -0400

    Update twilio.js

--- Binder_HackTJ/routes/twilio.js
+++ Binder_HackTJ/routes/twilio.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 
 
-const accountSid = 'AC62ba4b863427d89d2090f16de29a95a4'; 
-const authToken = '1ec5c3b34ad4eccb93a9825ca21844fb'; 
+const accountSid = ''; 
+const authToken = ''; 
 const client = require('twilio')(accountSid, authToken); 
  
 router.get('/send_message', function(req, res, next){
@@ -23,7 +23,7 @@ function sendMessage(phone) {
       client.messages 
       .create({ 
          body: 'A Tutee is interested in your service!',  
-         messagingServiceSid: 'MG8fd3943c37b774ba82e1b3b93c0b831d',      
+         messagingServiceSid: '',      
          to: '+1'+ phone 
        }) 
       .then(message => console.log(message.sid)) 
